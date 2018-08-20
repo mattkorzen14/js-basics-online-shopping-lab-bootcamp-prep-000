b4ccd55909e9816b1b763ba712792c62476f5992
Matthew Korzen <mattkorzen14@gmail.com>
2018-08-20 23:14:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,23 +8,75 @@ function setCart(c) {
   cart = c;
   return cart;
 }
-
 function addToCart(item) {
- // write your code here
+  var itemObj = {};
+  var price = Math.floor(Math.random()*100)+1;
+  itemObj = {[item]:price};
+  cart.push(itemObj);
+  //console.log(cart);
+  return item + " has been added to your cart."
 }
-
 function viewCart() {
-  // write your code here
+  if (cart.length===0) {
+    return "Your shopping cart is empty."
+  }
+  else {
+    var output = "In your cart, you have ";
+    let value = "";
+    for (var i = 0; i< cart.length; i++ ) {
+      //var object = cart[i];
+      //console.log(object);
+      output +=  `${cart[0]['itemName']} at $${cart[0]['price']}`;
+      if (i === cart.length-1) {
+        output += "."
+      }
+      else if(i===cart.length-2) {
+        output += ", and "
+      }
+      else {
+        output += ", "
+      }
+    }
+    console.log(output);
+    return output;
+  }
 }
 
 function total() {
-  // write your code here
+   var sum = 0;
+for (var i = 0; i< cart.length; i++ ) {
+      var object = cart[i];
+      sum += parseInt(Object.values(object)[0]);
+}
+//console.log(sum)
+return sum;
 }
 
-function removeFromCart(item) {
-  // write your code here
+function removeFromCart(itemName) {
+  for(var i=0;i<cart.length;i++) {
+    var object = cart[i];
+    if (Object.keys(object)[0] === itemName) {
+      cart.splice(i,1);
+     // console.log(cart);
+      return cart;
+    }
+  }
+ // console.log(cart);
+  return "That item is not in your cart."
 }
-
+function placeOrder() {
+  return "Sorry, we don't have a credit card on file for you."
+}
 function placeOrder(cardNumber) {
-  // write your code here
+  var cost = total();
+  cart = [];
+  console.log(`Your total cost is ${cost}, which will be charged to the card ${cardNumber}.`)
+  return `Your total cost is $${cost}, which will be charged to the card ${cardNumber}.`
 }
+
+addToCart("bananas");
+addToCart("nuts");
+addToCart("oatmeal");
+viewCart();
+removeFromCart("nuts");
+placeOrder(123123123);
