4708c905828fe962812524716847873ad3836075
Matthew Korzen <mattkorzen14@gmail.com>
2018-08-21 01:44:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function viewCart() {
     for (var i = 0; i< cart.length; i++ ) {
       //var object = cart[i];
       //console.log(object);
-      output +=  `${cart['itemName']} at $${cart[0]['itemPrice']}`;
+      output +=  `${cart['itemName']} at $${cart['itemPrice']}`;
       if (i === cart.length-1) {
         output += "."
       }
